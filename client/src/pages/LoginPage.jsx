58c577e0a1fc1d99df7bba1717b14d6cf0b8c9e5
Ali Munir <alimunir1445@gmail.com>
2025-02-18 15:52:43 +0500

    Update page title and add authenticated layer in login and signup pages

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Login from "../components/Login/Login.jsx";
+import { useSelector } from "react-redux";
+import {useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const { isAuthenticatedUser } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (isAuthenticatedUser === true) {
+      navigate("/");
+    }
+  }, []);
   return (
     <div>
       <Login />
--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SignUp from "../components/SignUp/SignUp";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const SignUpPage = () => {
+  const navigate = useNavigate();
+  const { isAuthenticatedUser } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (isAuthenticatedUser) {
+      navigate("/");
+    }
+  }, []);
   return (
     <div>
       <SignUp />
